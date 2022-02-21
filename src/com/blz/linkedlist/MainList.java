582eba420c801b079e6d1d040d9c98bf0c0facbd
KarthikBenki <karthikmc007@gmail.com>
2022-02-21 09:12:54 +0530

    Added: stack and queue methods

--- src/com/blz/linkedlist/MainList.java
+++ src/com/blz/linkedlist/MainList.java
@@ -4,14 +4,13 @@ public class MainList {
 
     public static void main(String[] args) {
 	LinkedList l = new LinkedList();
-	l.addFirst(10);
-	l.addFirst(20);
-	l.addFirst(30);
-	l.addLast(40);
-	l.displayList();
-	l.searchData(15);
-	int count = l.getSize();
-	System.out.println("count = " + count);
+	Queue_2 queue = new Queue_2();
+	queue.enque(10);
+	queue.enque(20);
+	queue.enque(30);
+	queue.showQueue();
+	queue.deque();
+	queue.showQueue();
 
     }
 
